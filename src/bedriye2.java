ca1645c898ebd5636e1f4aa6a1c51979fbd9ea99
bedriye1 <113391866+bedriye1@users.noreply.github.com>
2022-12-12 22:31:24 +0300

    Update bedriye2.java
    add some file please pull it

--- src/bedriye2.java
+++ src/bedriye2.java
@@ -2,7 +2,7 @@ public class bedriye2 {
     public static void main(String[] args) {
 
         System.out.println("hello word");
-        System.out.println("welcome to dubai");
+        System.out.println("welcome to dubai habibi");
 
 
     }
